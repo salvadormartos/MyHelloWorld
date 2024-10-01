cee9848b42a382e11d1a539108add4eca33c7f68
Salvador <salvador.martos@gmail.com>
2024-10-01 10:53:10 +0200

    Primeras lineas

--- app/src/main/java/com/smb/myhelloworld/MainActivity.kt
+++ app/src/main/java/com/smb/myhelloworld/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.smb.myhelloworld.ui.theme.MyHelloWorldTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,40 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyHelloWorldTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true, widthDp = 320
+)
+@Composable
+fun GreetingPreview() {
+    MyHelloWorldTheme {
+        MyApp()
+    }
+}
+
+@Composable
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column (modifier){
+        for (name in names) {
+            Greeting(name = name)
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyHelloWorldTheme {
-        Greeting("Android")
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Column(modifier = modifier.padding(24.dp)) {
+            Text(text = "Hello ")
+            Text(text = name)
+        }
     }
 }
